46c458b9f21ba9c56d43cf2267b454918440fab9
TimiTao <kunicki.tomasz@gmail.com>
2019-03-07 16:26:34 +0100

    update scripts

--- local-composer.sh
+++ local-composer.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
 
-STR=$1;
+app=${PWD}
+appVendor=$app/vendor
+if [ ! -f $app/composer.json ]; then
+    echo "$app/composer.json don't exist"
+    exit 128
+fi
 
-length=${#STR}
-last_char=${STR:length-1:1}
-[[ $last_char == "/" ]] && STR=${STR:0:length-1}; :
+DIR_NAME=${PWD##*/} # current dir name
 
-COMPOSER=/app/$STR/docker-composer.json \
-COMPOSER_VENDOR_DIR=/app/$STR/vendor \
-composer update
+length=${#DIR_NAME}
+last_char=${DIR_NAME:length-1:1}
+[[ $last_char == "/" ]] && DIR_NAME=${DIR_NAME:0:length-1}; :
+
+dockerComposerFileDir=/Development/composer-configs/$DIR_NAME
+dockerComposerFile=$dockerComposerFileDir/docker-composer.json
+if [ ! -f $dockerComposerFile ]; then
+    mkdir -p $dockerComposerFileDir
+    template=`cat /Development/template-composer.json`
+    fileContent=${template/REPLACE/$DIR_NAME}
+    echo "$fileContent" > "$dockerComposerFile"
+fi
+
+COMPOSER=$dockerComposerFile \
+COMPOSER_VENDOR_DIR=$appVendor \
+composer $1 $2 $3 $4 $5
