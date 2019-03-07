f46a7b0e0ce1d6e54e32f737ac251f1243328e68
TimiTao <kunicki.tomasz@gmail.com>
2019-03-07 20:30:29 +0100

    move config storage

--- local-composer.sh
+++ local-composer.sh
@@ -7,17 +7,25 @@ if [ ! -f $app/composer.json ]; then
     exit 128
 fi
 
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+
 DIR_NAME=${PWD##*/} # current dir name
 
 length=${#DIR_NAME}
 last_char=${DIR_NAME:length-1:1}
 [[ $last_char == "/" ]] && DIR_NAME=${DIR_NAME:0:length-1}; :
 
-dockerComposerFileDir=/Development/composer-configs/$DIR_NAME
+dockerComposerFileDir=$CONFIG_STORAGE/$DIR_NAME
 dockerComposerFile=$dockerComposerFileDir/docker-composer.json
 if [ ! -f $dockerComposerFile ]; then
     mkdir -p $dockerComposerFileDir
-    template=`cat /Development/template-composer.json`
+    template=`cat $MY_PATH/template-composer.json`
     fileContent=${template/REPLACE/$DIR_NAME}
     echo "$fileContent" > "$dockerComposerFile"
 fi
